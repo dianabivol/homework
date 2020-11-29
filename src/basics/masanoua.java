955cf7b8b9333aaffde22034462eedbfc6171da9
dianabivol <bivol.diana@gmail.com>
2020-11-29 19:42:36 +0200

    src/basics/masanoua.java

---
+++ src/basics/masanoua.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class masanoua {
+    public static void main(String[] args) {
+        System.out.println("x                       X");
+        System.out.println("x                       x");
+        System.out.println("x      XXXXXXXXXX       X");
+        System.out.println("xxxxx  X         X  xxxxx");
+        System.out.println("x   x  X         X  X   x");
+        System.out.println("x   x  x         x  x   x");
+    }
+}
